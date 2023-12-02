f07dfd80a70ffd7a81fe3978deed9e4aad63b4d4
Anderson Roque <andersonbarrinha18@gmail.com>
2023-12-01 23:04:55 -0300

    Feat:add brands animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client";
 
+import { BrandAnimation } from "@/components/BrandAnimation";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
+      <BrandAnimation />
       <Footer />
     </main>
   );
---
+++ src/components/BrandAnimation/index.jsx
@@ -0,0 +1,63 @@
+"use client";
+import Image from "next/image";
+import { useCallback, useEffect, useMemo, useRef } from "react";
+
+export function BrandAnimation() {
+  const scroller = useRef(null);
+  const scrollerInner = useRef(null);
+
+  const addAnimation = useCallback(() => {
+    const scrollerList = Array.from(scroller.current.children);
+    const scrollerContent = Array.from(scrollerInner.current.children);
+
+    scrollerList.forEach((item) => {
+      scroller.current.setAttribute("data-animated", true);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.current.appendChild(duplicatedItem);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (scroller.current && scrollerInner.current) {
+      addAnimation();
+    }
+  }, [scroller, scrollerInner, addAnimation]);
+
+  const brands = useMemo(
+    () => [
+      { path: "/discord.svg" },
+      { path: "/ifood.svg" },
+      { path: "/whatsapp-logo.png" },
+      { path: "/amazon.png" },
+      { path: "/ifood.svg" },
+      { path: "/amazon.png" },
+    ],
+    []
+  );
+
+  return (
+    <section className="w-full justify-center p-4 flex flex-col items-center gap-4">
+      <h1 className="font-bold text-3xl text-[#0A2E72] uppercase">
+        Marcas e Parceiros!
+      </h1>
+      <div className="scroller" ref={scroller}>
+        <ul
+          className="tag-list scroller__inner flex items-center"
+          ref={scrollerInner}
+        >
+          {brands.map((item, index) => {
+            return (
+              <li key={index}>
+                <Image src={item.path} alt="" width={100} height={50} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+}
